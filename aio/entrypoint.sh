60779393ca17a91763bb59305640ba5eae0ef21a
William Harrison <william@williamdavidharrison.com.au>
2024-04-19 19:43:15 +0800

    fix entrypoint + support pip upgrade

--- aio/entrypoint.sh
+++ aio/entrypoint.sh
@@ -6,8 +6,8 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
