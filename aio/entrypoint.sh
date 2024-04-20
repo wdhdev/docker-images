9a17c7af4043059d780f7652c460872d41150bf3
William Harrison <william@williamdavidharrison.com.au>
2024-04-20 08:00:11 +0800

    update aio entrypoint and workflow

--- aio/entrypoint.sh
+++ aio/entrypoint.sh
@@ -7,7 +7,7 @@ export INTERNAL_IP
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "$ ${MODIFIED_STARTUP}"
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
