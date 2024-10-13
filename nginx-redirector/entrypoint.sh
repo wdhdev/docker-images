acebe5387e80386355eb9939fa50896b3a191881
William Harrison <william@williamdavidharrison.com.au>
2024-10-13 12:25:57 +0800

    del

--- nginx-redirector/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-: ${REDIRECT_STATUS:=301}
-
-envsubst '$SERVER_NAME $REDIRECT_URL $REDIRECT_STATUS' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-
-nginx -g 'daemon off;'
